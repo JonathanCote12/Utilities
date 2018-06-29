d1806e0600c622b139695401f86e69e3604475ae
SergeDmi <serge.dmitrieff@gmail.com>
2018-06-29 10:23:39 +0200

    tools update

--- bin/splot.py
+++ bin/splot.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright Serge Dmitrieff 
+# Copyright Serge Dmitrieff
 # www.biophysics.fr
 #
 # Based on Python Pyx
-
 from pyx import *
 from numpy import *
 from pyx.graph import axis
@@ -16,21 +15,21 @@ from tools import *
 # SYNOPSIS
 
    splot is a shorthand command-line tool draw graphs using PyX. PyX is good.
-   
+
 # DESCRIPTION
-   
+
    splot plots a graph from a text file (should I add excel support ?)
    it is meant to be fast and dirty (but uses PyX to be beautiful)
-   
+
 # SYNTAX
-   
+
    python splot.py TEXT_FILE [OPTIONS] [ADDITIONAL_TEXT_FILES] [ADDITIONAL_OPTIONS]
 
 # OPTIONS
-	
+
 	splot has two kinds of options : global (for the whole figure) and local (for a particular file)
 	All options should be written as option_name=option_value
-	
+
 	Global options :
 		xlabel		: label of x axis
 		ylabel		: label of y axis
@@ -42,7 +41,7 @@ from tools import *
 		ymax		: max y value
 		key		: position of figure legend
 		out		: name of output file
-		
+
 	Local options :
 		x		: index of column or row to be used as x axis values (e.g. x=0 for the first column)
 						also can specify an operation : x=A[:,0]*A[:,1]
@@ -51,25 +50,25 @@ from tools import *
 		dy		: index of column or row to be used as dy values (e.g. x=0 for the first column)
 						also can specify an operation : dy=A[:,2]/sqrt(A[:,3])
 		mode		: h for horizontal (rows), v for vertical (column) (default)
-		
+
 		color		: color of lines or symbol ; can be either red, green, blue, dark, medium, light, black
 						or color.cmyk.*  or color.rgb.*
 						or an operation, e.g. color=A[:,2]
-		
+
 		style		: style of plot : - or _ for a line, -- for dashed, .- for dashdotted
-									o for circles  x , * for crosses  + for plus   > , <	 for triangles	
+									o for circles  x , * for crosses  + for plus   > , <	 for triangles
 		if		: condition to keep the rows or columns
 
 		range		: range of rows / columns to plot
 
-		size		: size of symbol used							
-									
-		line		: thickness of line, from 0 to 5 
-		
+		size		: size of symbol used
+
+		line		: thickness of line, from 0 to 5
+
 		title (or legend) : title of the graph
-													
+
 # EXAMPLES :
-			
+
 			splot.py file.txt
 						plots the second column of file.txt as a function of the first column
 			splot.py file.txt x=3 y=7
@@ -82,9 +81,9 @@ from tools import *
 						the deviation is set from the fourth column of file.txt
 						the color is set from the second column of file.txt, based on a gray-level gradient
 						labels and titles use the Latex interpreter
-			splot.py file.txt if='A[:,0]>1' 
+			splot.py file.txt if='A[:,0]>1'
 						plots the second column as a function of the first if elements of the first are greater than 1
-			splot.py file.txt mode='h' if='A[0,:]>1' 
+			splot.py file.txt mode='h' if='A[0,:]>1'
 						plots the second row as a function of the first row if elements of the first row are greater than 1
 """
 
@@ -103,7 +102,7 @@ col_dict= {
 	'medium' : color.gray(0.5),
 	'light' : color.gray(0.75)
 	}
-	
+
 linst_dict={
 	'_' : style.linestyle.solid,
 	'-' : style.linestyle.solid,
@@ -121,7 +120,7 @@ symst_dict={
 	'>' : graph.style.symbol.triangle,
 	'<' : graph.style.symbol.triangle
 	}
-	
+
 linw_dict={
 	'1' : 	style.linewidth.thin,
 	'2' : 	style.linewidth.thick,
@@ -129,7 +128,7 @@ linw_dict={
 	'4' : 	style.linewidth.THIck,
 	'5' : 	style.linewidth.THICK
 	}
-	
+
 grad_dict={
 	'rainbow'	: 	color.gradient.Rainbow,
 	'whitered'	: 	color.gradient.WhiteRed,
@@ -140,8 +139,8 @@ grad_dict={
 	'grey'		: 	color.gradient.Gray,
 	'gr'		: 	color.gradient.Gray,
 	'jet'		: 	color.gradient.Jet,
-	}	
-	
+	}
+
 class Glob:
 	def __init__(self, args):
 		narg=len(args)
@@ -157,16 +156,16 @@ class Glob:
 		self.key=None
 		self.width=8
 		self.height=5
-		self.kdist=0.1	
-		keyz=''	
+		self.kdist=0.1
+		keyz=''
 		files=[]
 		for i,arg in enumerate(args):
 			if arg.startswith('out='):
 				self.out=arg[4:]
 			elif arg.startswith('xlabel='):
-				self.xlabel=arg[7:]	
+				self.xlabel=arg[7:]
 			elif arg.startswith('ylabel='):
-				self.ylabel=arg[7:]	
+				self.ylabel=arg[7:]
 			elif arg.startswith('width='):
 				self.width=float(arg[6:])
 			elif arg.startswith('height='):
@@ -182,40 +181,40 @@ class Glob:
 			elif arg.startswith('key='):
 				keyz=arg[4:]
 			elif arg.startswith('kdist='):
-				self.kdist=arg[6:]	
+				self.kdist=arg[6:]
 			elif arg.startswith('legend=') or arg.startswith('title'):
 				self.key=graph.key.key(pos="tl", dist=self.kdist)
-			elif arg.startswith('--help'):	
+			elif arg.startswith('--help'):
 				self.usage()
 			elif arg.find('=')<0:
-				files.append(i)		
+				files.append(i)
 
 		if self.xlabel:
 			try:
 				self.xlabel=r"%s" %(self.xlabel)
 			except:
 				self.xlabel=None
-			
+
 		if self.ylabel:
 			try:
 				self.ylabel=r"%s" %(self.ylabel)
 			except:
 				self.ylabel=None
-		
+
 		try:
 			self.key=graph.key.key(pos="%s" %(keyz), dist=float(self.kdist))
 		except:
 			if keyz=='None':
 				self.key=None
-		
-		self.graph=graph.graphxy(width=self.width,height=self.height,key=self.key,x=axis.linear(title=self.xlabel,min=self.xmin,max=self.xmax),y=axis.linear(title=self.ylabel,min=self.ymin,max=self.ymax))				
+
+		self.graph=graph.graphxy(width=self.width,height=self.height,key=self.key,x=axis.linear(title=self.xlabel,min=self.xmin,max=self.xmax),y=axis.linear(title=self.ylabel,min=self.ymin,max=self.ymax))
 		files.append(narg)
 		nf=len(files)-1
 		self.graphs=[Graph(args[files[i]:files[i+1]]) for i in range(nf)]
 
 	def make_plot(self):
 		for graf in self.graphs:
-			self.plot(graf)	
+			self.plot(graf)
 
 	def plot(self,graf):
 		self.graph.plot([graph.data.points([(x,graf.Y[i],graf.dX[i],graf.dY[i],graf.S[i],graf.C[i]) for i, x in enumerate(graf.X[:])], x=1, y=2,dx=3,dy=4,size=5,color=6,title=graf.legend)],graf.style)
@@ -223,7 +222,7 @@ class Glob:
 	def save_plot(self):
 		if self.graphs:
 			if self.out.endswith('.eps'):
-				self.graph.writeEPSfile(self.out)	
+				self.graph.writeEPSfile(self.out)
 			elif self.out.endswith('.svg'):
 				self.graph.writeSVGfile(self.out)
 			else:
@@ -238,7 +237,7 @@ class Glob:
 		disp('splot.py file.txt color=red file2.txt out=plot.pdf')
 		disp('splot.py file.txt y=A[:,1]^2+A[:,2]^2 dy=3 color=1')
 		quit
-			
+
 class Graph(Glob):
 	numr=-1
 	def __init__(self, args):
@@ -252,22 +251,24 @@ class Graph(Glob):
 		self.dX=[]
 		self.dY=[]
 		self.S=[]
-		self.C=[]		
+		self.C=[]
 		self.dx=[]
-		self.dy=[]		
-		self.col=[]
+		self.dy=[]
+		#self.col=[]
+		col=''
+		siz=''
 		self.cond=[]
-		self.range=[]	
+		self.range=[]
 		(A,a,b)=getdata(self.file)
 		labels=splitheader(self.file)
-		
+
 		# Dirty tricks for maximum compatibility
 		if min(a,b)==1:
 			self.x='auto'
-			self.y=0	
+			self.y=0
 		if a==1:
 			self.mode='h'
-		
+
 		for arg in args:
 			if arg.startswith('legend=') or arg.startswith('title='):
 				if arg.startswith('legend='):
@@ -279,7 +280,7 @@ class Graph(Glob):
 				else:
 					self.legend=r"%s" %legend
 			elif arg.startswith('x='):
-				self.x=(arg[2:])	
+				self.x=(arg[2:])
 			elif arg.startswith('y='):
 				self.y=(arg[2:])
 			elif arg.startswith('mode='):
@@ -294,27 +295,29 @@ class Graph(Glob):
 				self.range=(arg[6:])
 			elif arg.startswith('color='):
 				col=arg[6:]
-				if col.isdigit() or col.find('A[')>=0:
-					self.C=self.set_from_input(A,col,'color')
 			elif arg.startswith('size='):
 				siz=arg[5:]
-				if siz.isdigit() or siz.find('A[')>=0:
-					self.S=self.set_from_input(A,siz,'size')	
-		
+
+
 		if (len(self.range) or len(self.cond)):
-			A=self.set_A_range(A)	
-	
+			A=self.set_A_range(A)
+
+		if siz.isdigit() or siz.find('A[')>=0:
+			self.S=self.set_from_input(A,siz,'size')
+		if col.isdigit() or col.find('A[')>=0:
+			self.C=self.set_from_input(A,col,'color')
+
 		self.X=self.set_from_input(A,self.x,'x')
 		self.Y=self.set_from_input(A,self.y,'y')
 		self.dX=self.set_from_input(A,self.dx,'dx')
 		self.dY=self.set_from_input(A,self.dy,'dy')
 
-		
+
 		if not len(self.C):
 			self.C=self.X
 		if not len(self.S):
 			self.S=self.X
-		
+
 
 		lX=len(self.X)
 		lY=len(self.Y)
@@ -327,21 +330,21 @@ class Graph(Glob):
 		if not len(self.dY):
 			self.dY=zeros((lX,1))
 		if not len(self.dX):
-			self.dX=zeros((lX,1))	
+			self.dX=zeros((lX,1))
 
 		self.C=(self.C-min(self.C))/(max(self.C)-min(self.C))
-		
+
 		if min(self.S)<=0:
 			self.S=(self.S-min(self.S))+0.001
-		
+
 		self.style=Style(args).style
-				
+
 	def set_from_input(self,A,input,coord):
 		# We first check if axis defined by a row/column number
-		try :			
+		try :
 			i=int(input)
 			if self.mode=='h':
-				return A[i,:]				
+				return A[i,:]
 			else:
 				return A[:,i]
 		except:
@@ -380,7 +383,7 @@ class Graph(Glob):
 					print('Range must be of the format begin:end or begin:range')
 					raise ValueError('Range must be of the format begin:end or begin:step:end')
 			except:
-				raise ValueError('Cannot convert Range to adequate format (note : range must be of the format begin:end or begin:step:end)')		
+				raise ValueError('Cannot convert Range to adequate format (note : range must be of the format begin:end or begin:step:end)')
 		# Now we transpose for the 'if' operation or export
 		if self.mode=='h':
 			A=B.transpose()
@@ -395,24 +398,24 @@ class Graph(Glob):
 				else:
 					A=A[kept]
 			except:
-				raise ValueError('Cannot understand condition. Hint use : if=\'A[:,2]>0.5\' ')	
+				raise ValueError('Cannot understand condition. Hint use : if=\'A[:,2]>0.5\' ')
 		return A
-		
+
 class Style(Graph):
 	def __init__(self, args):
 		count=Graph.numr
 		self.style=[]
 		self.dxy=[]
-		self.goodstyle=goodstyle(args,count)	
-		
+		self.goodstyle=goodstyle(args,count)
+
 		for arg in args:
 			if arg.startswith('dy=') or arg.startswith('dx='):
 				if self.goodstyle.setcolor:
 					self.dxy=[self.goodstyle.linew,self.goodstyle.setcolor]
-				else:	
+				else:
 					self.dxy=[self.goodstyle.linew,colours[0]]
-					
-					
+
+
 		if self.goodstyle.kind=='symbol':
 			if not len(self.dxy):
 				self.style=[changesymbol(**vars(self.goodstyle)),graph.style.errorbar(False)]
@@ -424,7 +427,7 @@ class Style(Graph):
 				self.style=[graph.style.line([self.goodstyle.linest,self.goodstyle.linew,self.goodstyle.setcolor]),graph.style.errorbar(False)]
 			else:
 				self.style=[graph.style.line([self.goodstyle.linest,self.goodstyle.linew,self.goodstyle.setcolor]),graph.style.errorbar(errorbarattrs=self.dxy)]
-				
+
 
 class goodstyle(Style):
 	def __init__(self,args,count):
@@ -435,14 +438,14 @@ class goodstyle(Style):
 		self.linew=style.linewidth.thin
 		self.linest=linests[count %4]
 		self.gradient=color.gradient.Rainbow;
-		
-		for arg in args:	
+
+		for arg in args:
 			if arg.startswith('color='):
 				col=arg[6:]
 				try :
 					# We first try to set it from the dictionary
 					self.setcolor=col_dict[col]
-				except :	
+				except :
 					if col.isdigit() or col.find('A[')>=0:
 						# Color should be set from the data
 						self.setcolor=False
@@ -456,13 +459,13 @@ class goodstyle(Style):
 							self.setcolor=eval(col)
 						except:
 							print('Warning : could not understand color from %s' %col)
-			
+
 			if arg.startswith('gradient=') or arg.startswith('grad='):
 				grad=arg.split('=')[1]
 				try :
 					# We first try to set it from the dictionary
 					self.gradient=grad_dict[grad]
-				except :	
+				except :
 					if not grad.startswith('color.gradient'):
 						# shorthand notation is tolerated
 						if grad.startswith('gradient'):
@@ -474,12 +477,12 @@ class goodstyle(Style):
 						self.gradient=eval(grad)
 					except:
 						print('Warning : could not understand gradient from %s' %grad)
-			
+
 			elif arg.startswith('size='):
 				siz=arg[5:]
 				if siz.find('A[')>=0:
 					# Size depends on data
-					self.setsize=-1					
+					self.setsize=-1
 				else:
 					try:
 						sizi=float(siz)
@@ -491,7 +494,7 @@ class goodstyle(Style):
 							self.setsize=sizi
 					except:
 						print('Warning : could not understand size from %s' %siz)
-						
+
 			elif arg.startswith('line='):
 				self.kind='line'
 				lin=arg[5:]
@@ -500,7 +503,7 @@ class goodstyle(Style):
 				except:
 					print('Warning : could not understand line width from %s' %lin)
 
-			elif arg.startswith('style='):		
+			elif arg.startswith('style='):
 				stil=arg[6:]
 				try:
 					self.linest=linst_dict[stil]
@@ -511,12 +514,12 @@ class goodstyle(Style):
 						self.kind='symbol'
 					except:
 						print('Warning : could not understand style from %s' %stil)
-					
+
 		if self.kind=='line':
 			# For now splot does not support gradient line coloring
 			if not self.setcolor:
 				self.setcolor=colours[int(ceil(count/4)) %4]
-							
+
 
 
 class changesymbol(graph.style.symbol):
@@ -551,7 +554,7 @@ class changesymbol(graph.style.symbol):
     def drawpoint(self, privatedata, sharedata, graph, point):
         # replace the original drawpoint method by a slightly revised one
         if sharedata.vposvalid and privatedata.symbolattrs is not None:
-            x_pt, y_pt = graph.vpos_pt(*sharedata.vpos)            
+            x_pt, y_pt = graph.vpos_pt(*sharedata.vpos)
             if self.setsize<0:
             	siz=privatedata.size_pt*point[self.sizecolumnname]
             else :
@@ -563,7 +566,7 @@ class changesymbol(graph.style.symbol):
             col =privatedata.symbolattrs + [color]
             privatedata.symbol(privatedata.symbolcanvas, x_pt, y_pt, siz, col)
 
-		
+
 if __name__ == "__main__":
 	nargs=len(sys.argv);
 	args=sys.argv[1:];
@@ -571,4 +574,3 @@ if __name__ == "__main__":
 	glob=Glob(args)
 	glob.make_plot()
 	glob.save_plot()
-
